4a92e2ef4ad5e9508cda545e86daf24ac67cc585
gyutae <qoxowkd0716@gmail.com>
2024-04-29 15:34:31 +0900

    Refactor: RestTemplate 설정 변경

--- modules/internal/weather-service/src/main/java/com/example/weather_forecast/service/RestClientConfig.java
+++ modules/internal/weather-service/src/main/java/com/example/weather_forecast/service/RestClientConfig.java
@@ -2,6 +2,8 @@ package com.example.weather_forecast.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +11,14 @@ public class RestClientConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+
+        // JSON 메시지 컨버터 추가
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        // XML 메시지 컨버터 추가
+        restTemplate.getMessageConverters().add(new MappingJackson2XmlHttpMessageConverter());
+
+        return restTemplate;
     }
 }
